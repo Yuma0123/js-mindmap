c24eb604d33e31d060e9856ad09213906a00a893
Yuma0123 <yuma205kuri123@gmail.com>
2024-06-11 13:37:34 +0900

    デフォルトのコード

--- script.js
+++ script.js
@@ -1,41 +1,43 @@
-// load the mindmap
 $(document).ready(function() {
-  // enable the mindmap in the body
-  $('body').mindmap();
+  // 各マインドマップコンテナに対して個別にマインドマップを初期化する
+  $('.mindmap-container').each(function() {
+    $(this).mindmap();
 
-  // add the data to the mindmap
-  var root = $('body>ul>li').get(0).mynode = $('body').addRootNode($('body>ul>li>a').text(), {
-    href:'/',
-    url:'/',
-    onclick:function(node) {
-      $(node.obj.activeNode.content).each(function() {
-        this.hide();
-      });
-    }
-  });
-  $('body>ul>li').hide();
-  var addLI = function() {
-    var parentnode = $(this).parents('li').get(0);
-    if (typeof(parentnode)=='undefined') parentnode=root;
-      else parentnode=parentnode.mynode;
-    
-    this.mynode = $('body').addNode(parentnode, $('a:eq(0)',this).text(), {
-//          href:$('a:eq(0)',this).text().toLowerCase(),
-      href:$('a:eq(0)',this).attr('href'),
-      onclick:function(node) {
+    var root = $(this).find('>ul>li').get(0).mynode = $(this).addRootNode($(this).find('>ul>li>a').text(), {
+      href: '/',
+      url: '/',
+      onclick: function(node) {
         $(node.obj.activeNode.content).each(function() {
           this.hide();
         });
-        $(node.content).each(function() {
-          this.show();
-        });
       }
     });
-    $(this).hide();
-    $('>ul>li', this).each(addLI);
-  };
-  $('body>ul>li>ul').each(function() { 
-    $('>li', this).each(addLI);
-  });
 
-});   
+    $(this).find('>ul>li').hide();
+
+    var addLI = function() {
+      var parentnode = $(this).parents('li').get(0);
+      if (typeof(parentnode) == 'undefined') parentnode = root;
+      else parentnode = parentnode.mynode;
+
+      this.mynode = $(this).addNode(parentnode, $('a:eq(0)', this).text(), {
+        href: $('a:eq(0)', this).attr('href'),
+        onclick: function(node) {
+          $(node.obj.activeNode.content).each(function() {
+            this.hide();
+          });
+          $(node.content).each(function() {
+            this.show();
+          });
+        }
+      });
+      $(this).hide();
+      $('>ul>li', this).each(addLI);
+    };
+
+    $(this).find('>ul>li>ul').each(function() {
+      $('>li', this).each(addLI);
+    });
+
+  });
+});
